9993b5f7f31428270e880a2968cfebad6b1ff5e4
Antonio Felix <afelixjdev@gmail.com>
2024-11-08 13:33:56 -0500

    3d portfolio project setup + navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from './sections/NavBar'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+    
+      <NavBar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
