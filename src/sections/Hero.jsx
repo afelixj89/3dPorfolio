6e3718abc10b51966c711b83113d748a6e051a46
Antonio Felix <afelixjdev@gmail.com>
2024-11-10 18:44:23 -0500

    added 3d hackerrom model to the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
 
 function Hero() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hello, I am Antonio! <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Building Web Applications</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={0.07}
+              position={[0, -1, 0]}
+              rotation={[0, 280, 0]}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
