349c44cf4e9a8b788e0eeb00b115367ae4e0a0ba
Antonio Felix <afelixjdev@gmail.com>
2024-11-11 01:20:11 -0500

    scaled 3d model for the hackerroom in the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,52 @@ import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
 
 function Hero() {
+  const x = useControls("HackerRoom", {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+  });
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -14,14 +58,19 @@ function Hero() {
         <p className="hero_tag text-gray_gradient">Building Web Applications</p>
       </div>
       <div className="w-full h-full absolute inset-0">
+        <Leva />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              scale={0.07}
-              position={[0, -1, 0]}
-              rotation={[0, 280, 0]}
+              scale={isMobile ? 0.07 : 0.1}
+              rotation={[0.2, -3.2, 0]}
+              position={[1, -4.5, -0.5]}
+              // position={[x.positionX, x.positionY, x.positionZ]}
+              // rotation={[x.rotationX, x.rotationY, x.rotationZ]}
+              // scale={[x.scale, x.scale, x.scale]}
             />
+
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
