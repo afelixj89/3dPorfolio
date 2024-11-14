e33867e6c82bed341538910f8c87bbae4fcb71d2
Antonio Felix <afelixjdev@gmail.com>
2024-11-13 19:25:59 -0500

    added an intitial project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './sections/NavBar'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import Projects from './sections/Projects'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
